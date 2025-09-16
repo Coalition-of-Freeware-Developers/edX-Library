bb651355d0ae9497283c44b584ca32429ad96aed
Thomas Ray <the3dvehicleguy@gmail.com>
2025-09-16 14:07:02 -0400

    update files with proper license headers

--- edX/config/edXConfig.h
+++ edX/config/edXConfig.h
@@ -1,9 +1,22 @@
 ﻿/**
 * -------------------------------------------------------
-* Scenery Editor X - edX File Format
-* -------------------------------------------------------
 * Copyright (c) 2025 Thomas Ray
 * Copyright (c) 2025 Coalition of Freeware Developers
+* -------------------------------------------------------
+* Mozilla Public License Version 2.0
+* -------------------------------------------------------
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
 * -------------------------------------------------------
 * edXConfig.h
 * -------------------------------------------------------
@@ -21,23 +34,36 @@
 #include <nlohmann/json.hpp>
 
 /// ----------------------------------------------------------------------------
+// Detect platform
+#if defined _WIN32 || defined __CYGWIN__
+  #define EDX_PLATFORM_WINDOWS
+#elif defined(__APPLE__)
+  #define EDX_PLATFORM_MACOS
+#elif defined(__linux__)
+  #define EDX_PLATFORM_LINUX
+#endif
+
+/// ----------------------------------------------------------------------------
 
 // Cross-platform DLL export/import macros
-#if defined(_WIN32) || defined(_WIN64)
+#if defined(EDX_PLATFORM_WINDOWS)
     #ifdef EDX_EXPORTS
         #define EDX_API __declspec(dllexport)
     #else
         #define EDX_API __declspec(dllimport)
-    #endif
-#elif defined(__GNUC__) && __GNUC__ >= 4
-    #ifdef EDX_EXPORTS
-        #define EDX_API __attribute__((visibility("default")))
-    #else
-        #define EDX_API
-    #endif
+  #endif
+  #define EDX_LOCAL
 #else
+  #if __GNUC__ >= 4
+    #define EDX_API __attribute__((visibility("default")))
+    #define EDX_LOCAL __attribute__((visibility("hidden")))
+  #else
     #define EDX_API
+    #define EDX_LOCAL
+  #endif
 #endif
+
+/// ----------------------------------------------------------------------------
 
 // JSON namespace alias for convenience
 using json = nlohmann::json;
